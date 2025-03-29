1c0713e3d3bd8d9cc50e75ab3a1b2c4e9857ced0
brunoandradd <bruno77sa@gmail.com>
2025-03-29 13:40:43 -0300

    feat: show diff in terminal

--- gitconfig.sh
+++ gitconfig.sh
@@ -3,6 +3,8 @@ read -p "Informe o email do usuario git: " email
 git config --global user.name "$username"
 git config --global user.email $email
 
+git config --global core.pager "cat"
+
 git config --global color.branch auto
 git config --global color.diff auto
 git config --global color.interactive auto
@@ -12,6 +14,8 @@ git config --global alias.st status
 git config --global alias.ci commit
 git config --global alias.co checkout
 git config --global alias.br branch
+
+git@github.com:brunoandradd/gitconfig.git
 
 set mainfont {Monaco 12}
 set textfont {Monaco 12}
